3950243a497322ee7cf16a0a0204bd240bfea62d
okiidokim <127471879+okiidokim@users.noreply.github.com>
2023-11-23 20:14:10 +0900

    Revert "Revert "[FE] feat : 로그인 페이지 css 수정 및 개인 정보 페이지 patch 메소드 제외 구현 완료""

--- catch-culture-frontend/src/pages/mypage/Mypage.jsx
+++ catch-culture-frontend/src/pages/mypage/Mypage.jsx
@@ -34,7 +34,7 @@ function Mypage() {
             <NavLink to="/notify">
               <button className="notify">
                 <PiNotificationBold size="40" />
-                공지
+                제보
               </button>
             </NavLink>
             <NavLink to="/point-history">
