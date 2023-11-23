3ee1afb8dc1fd75acfb788a20f7e768c12019481
jinwoo1234 <skdiwlsdn5@naver.com>
2023-11-23 14:38:14 +0900

    Revert "Design: 방문인증 페이지 꾸미기"
    This reverts commit 0d308cecdbb44537c85cde250a597a612f71de80.

--- catch-culture-frontend/src/pages/visitAuth/VisitAuth.jsx
+++ catch-culture-frontend/src/pages/visitAuth/VisitAuth.jsx
@@ -2,13 +2,7 @@ import React, { useMemo, useState } from 'react';
 import { IoIosArrowBack } from 'react-icons/io';
 import * as S from './VisitAuthStyle';
 
-import Backitem from '../../components/Backitem';
-import UploadBox from '../../components/uploadImg/UploadBox'
-
 function VisitAuth() {
-
-    const [isAuthenticated, setIsAuth] = useState();
-
     const onClickBackButton = () => {
         navigate(-1);
     }
@@ -16,26 +10,14 @@ function VisitAuth() {
     return (
         <S.Wrapper>
             <S.Header>
-                <Backitem> 
+                <S.BackButton onClick={onClickBackButton}> 
                     <IoIosArrowBack />
-                </Backitem>
+                </S.BackButton>
+                
             </S.Header>
+            {/* <S.InfoArea>
 
-            <S.Container>
-                <S.TitleArea>
-                    더 크림 갤러리
-                </S.TitleArea>
-                <S.SubTitle>
-                    사진 등록 (최대 3개)
-                </S.SubTitle>
-                <UploadBox/>
-            </S.Container>
-
-            <S.ButtonSection>           
-                <button onClick={''} disabled={isAuthenticated} style={ isAuthenticated ? {backgroundColor: '#A7A7A7'} : {backgroundColor: '#018C0D'}}>
-                    방문 인증 요청
-                </button>
-            </S.ButtonSection>
+            </S.InfoArea> */}
         </S.Wrapper>
     )
 }
